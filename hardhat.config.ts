8ef290b7ab3ab660f5a9bf9e36dc1911895d2b7b
velopace <velopace7@gmail.com>
2024-12-21 00:23:19 +0800

    deploy and verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
+
+const BSC_TESTNET_URL = process.env.BSC_TESTNET_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const BSCSCAN_API_KEY = process.env.BSCSCAN_API_KEY
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    bsc_testnet: {
+      url: BSC_TESTNET_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: BSCSCAN_API_KEY,
+  }
 };
 
 export default config;
