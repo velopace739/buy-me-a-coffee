c8903f5751a1cb6db0ead5a55084bd9eb94797c7
velopace <velopace7@gmail.com>
2024-12-20 21:51:23 +0800

    deploy to hardhat net

---
+++ scripts/deploy.ts
@@ -0,0 +1,20 @@
+// scripts/deploy.ts
+
+import { ethers } from "hardhat";
+
+async function main() {
+  // We get the contract to deploy.
+  const BuyMeACoffee = await ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+
+  console.log("BuyMeACoffee deployed to:", await buyMeACoffee.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
